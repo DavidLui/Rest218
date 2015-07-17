f4c302db685405b46784bf1f91476299633521f9
DavidLui <dlui93@yahoo.com>
2015-07-17 18:00:34 -0400

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704230634) do
+ActiveRecord::Schema.define(version: 20150717182158) do
 
   create_table "appetizers", force: true do |t|
     t.string   "name"
@@ -169,6 +169,24 @@ ActiveRecord::Schema.define(version: 20150704230634) do
   end
 
   add_index "soups", ["name"], name: "index_soups_on_name"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vegetables", force: true do |t|
     t.string   "name"
