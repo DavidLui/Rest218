51851360a5e273e75331b15d88cd8b4c76e92d06
DavidLui <dlui93@yahoo.com>
2015-07-24 17:16:13 -0400

    all

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,9 +13,9 @@ class PinsController < ApplicationController
   # GET /pins/1.json
   def show
   end
-
+  #parameters
   def pin_params
-    params.require(:pin).permit(:description, :image)
+    params.require(:pin).permit(:name, :image)
   end
 
   # GET /pins/new
@@ -73,8 +73,4 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:name, :image)
-    end
 end
